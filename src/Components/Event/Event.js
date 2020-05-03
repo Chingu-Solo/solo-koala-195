29594bb154dc42095856287a053d2bf9977dc012
Yassin Bahassane <yassinfreethinker@gmail.com>
2020-05-03 11:27:41 +0000

    Event section background && SingnIn component

--- src/Components/Event/Event.js
+++ src/Components/Event/Event.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import SingnIn from "./SignIn";
 import classes from "./Event.module.css";
 
 class Event extends Component {
 	render() {
 		return (
 			<div className={classes.container}>
-				<h2 className={classes.color}>Event</h2>
+				<div className={classes.transBack} />
+				<SingnIn />
 			</div>
 		);
 	}
---
+++ src/Components/Event/SignIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./SignIn.module.css";
+
+const SignIn = () => {
+	return (
+		<div className={classes.container}>
+			<button>Sign in</button>
+		</div>
+	);
+};
+
+export default SignIn;
