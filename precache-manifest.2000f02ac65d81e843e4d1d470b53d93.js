fcd5d2963fb805c5418d4740fca0a9b735630a32
Yassin Bahassane <yassinfreethinker@gmail.com>
2020-05-07 13:06:06 +0000

    Updates

---
+++ precache-manifest.2000f02ac65d81e843e4d1d470b53d93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d19b0c209bf28be0001d6d04713060",
+    "url": "/solo-koala-195/index.html"
+  },
+  {
+    "revision": "7460e13f3ad07edacbce",
+    "url": "/solo-koala-195/static/css/main.e743802e.chunk.css"
+  },
+  {
+    "revision": "781c9646c9940b20cc91",
+    "url": "/solo-koala-195/static/js/2.55ad2edc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/solo-koala-195/static/js/2.55ad2edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7460e13f3ad07edacbce",
+    "url": "/solo-koala-195/static/js/main.a673923f.chunk.js"
+  },
+  {
+    "revision": "497187c83e3638c73e5f",
+    "url": "/solo-koala-195/static/js/runtime-main.c6f5ec08.js"
+  }
+]);
